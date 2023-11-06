20434a2522cc480b4b78a969f7ce74f3e7ed55a9
SYED FAISAL <21052295@kiit.ac.in>
2023-11-06 19:24:12 +0530

    ImageOptimization.java

--- ImageOptimization.java
+++ ImageOptimization.java
@@ -15,10 +15,11 @@ public class ImageOptimization
 
             JFileChooser fileChooser = new JFileChooser();
             int returnValue = fileChooser.showOpenDialog(null);
-
-            if (returnValue == JFileChooser.APPROVE_OPTION) {
+            if (returnValue == JFileChooser.APPROVE_OPTION) 
+	    {
                 File selectedFile = fileChooser.getSelectedFile();
-                try {
+                try 
+		{
                     BufferedImage targetImage = ImageIO.read(selectedFile);
                     int width = targetImage.getWidth();
                     int height = targetImage.getHeight();
@@ -27,40 +28,40 @@ public class ImageOptimization
 
                     BufferedImage[] population = new BufferedImage[50];							// Create an initial population of images (chromosomes)
                     for (int i = 0; i < population.length; i++) 
-					{
+		    {
                         population[i] = copyImage(targetImage);
                     }
 
-                    for (int generation = 0; generation < 100; generation++) 
-					{
+                    for (int generation = 0; generation < 100; generation++) 			
+		    {
                         double[] fitness = new double[population.length];						// Evaluate the fitness of each image in the population
                         for (int i = 0; i < population.length; i++) 
-						{
+			{
                             BufferedImage currentImage = population[i];
                             fitness[i] = calculateFitness(currentImage, targetImage);
                             if (fitness[i] < bestFitness) 
-							{
+			    {
                                 bestFitness = fitness[i];
                                 bestImage = currentImage;
                             }
                         }
 						
-                        BufferedImage[] parents = selectParents(population, fitness);			// Select parents based on fitness
+                        BufferedImage[] parents = selectParents(population, fitness);					// Select parents based on fitness
                         
-						BufferedImage[] newGeneration = crossover(parents);						// Apply crossover to create a new generation
+			BufferedImage[] newGeneration = crossover(parents);						// Apply crossover to create a new generation
 
-                        newGeneration = mutate(newGeneration);									// Apply mutation to the new generation
+                        newGeneration = mutate(newGeneration);								// Apply mutation to the new generation
 
                         population = newGeneration;				                        		// Replace the old population with the new generation
                     }
 
                     if (bestImage != null) 
-					{
+		    {
                         JLabel label = new JLabel(new ImageIcon(bestImage));
                         frame.getContentPane().add(label, BorderLayout.CENTER);
                     }
                 } catch (IOException e) 
-				{
+		{
                     e.printStackTrace();
                 }
             }
@@ -69,52 +70,52 @@ public class ImageOptimization
         });
     }
 
-    private static BufferedImage copyImage(BufferedImage source)									// Creating a copy of an image	 
+    private static BufferedImage copyImage(BufferedImage source)							// Creating a copy of an image	 
 	{
-        int width = source.getWidth();
-        int height = source.getHeight();
-        BufferedImage copy = new BufferedImage(width, height, source.getType());
-        Graphics g = copy.getGraphics();
-        g.drawImage(source, 0, 0, null);
-        g.dispose();
-        return copy;
-    }
+		int width = source.getWidth();
+	        int height = source.getHeight();
+	        BufferedImage copy = new BufferedImage(width, height, source.getType());
+	        Graphics g = copy.getGraphics();
+	        g.drawImage(source, 0, 0, null);
+	        g.dispose();
+	        return copy;
+   	}
 
-    private static double calculateFitness(BufferedImage image, BufferedImage targetImage) 			// Calculate fitness (how similar the generated image is to the target image)
+    private static double calculateFitness(BufferedImage image, BufferedImage targetImage) 				// Calculate fitness (how similar the generated image is to the target image)
 	{
-        if (image == null) 
+        	if (image == null) 
 		{
-            return Double.MAX_VALUE;
-        }
+            		return Double.MAX_VALUE;
+        	}
         double error = 0.0;
         int width = image.getWidth();
         int height = image.getHeight();
 
         for (int y = 0; y < height; y++) 
-		{
+	{
             for (int x = 0; x < width; x++) 
-			{
+		{
                 int pixelImage = image.getRGB(x, y);
                 int pixelTarget = targetImage.getRGB(x, y);
                 int diff = pixelImage - pixelTarget;
                 error += diff * diff;
-            }
+           	}
         }
         return error;
-    }
+     }
 
     private static BufferedImage[] selectParents(BufferedImage[] population, double[] fitness)		// Select parents for the next generation based on fitness
 	{
-        return new BufferedImage[2]; // Return two parent images								    // Implement a selection method (e.g., roulette wheel or tournament selection)
-    }
+        	return new BufferedImage[2]; // Return two parent images								    // Implement a selection method (e.g., roulette wheel or tournament selection)
+    	}
 
     private static BufferedImage[] crossover(BufferedImage[] parents) 								// Apply crossover (recombination) to create a new generation
 	{
-        return new BufferedImage[parents.length];													// Implement a crossover method (e.g., one-point or two-point crossover)
-    }
+        	return new BufferedImage[parents.length];													// Implement a crossover method (e.g., one-point or two-point crossover)
+   	}
 	
     private static BufferedImage[] mutate(BufferedImage[] generation) 								// Apply mutation to the new generation
 	{
-        return generation;																			// Implement a mutation method (e.g., random pixel changes)
-    }
+        	return generation;																			// Implement a mutation method (e.g., random pixel changes)
+    	}
 }
